1aea5865a4fd9790dbae934809df10640eb101f2
ejones-cn <ejones.cn@hotmail.com>
2026-04-16 19:11:14 +0800

    refactor: 从ip_subnet_calculator导入handle_ip_subnet_error

--- ipam_sqlite.py
+++ ipam_sqlite.py
@@ -18,7 +18,7 @@ from i18n import translate as _
 from validators import IPAMValidator
 
 import ipaddress
-from ip_subnet_calculator import ip_to_int
+from ip_subnet_calculator import ip_to_int, handle_ip_subnet_error
 
 
 class IPAMSQLite:
